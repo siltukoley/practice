a77123dcff7b50309722f48bf3cbf3cbbbbe276a
siltukoley <siltukoley621@gmail.com>
2021-03-31 10:25:22 +0530

    test

--- siltu/home/views.py
+++ siltu/home/views.py
@@ -11,7 +11,7 @@ def contact(request):
         email = request.POST['email']
         phone=request.POST['phone']
         content =request.POST['content']
-        contact.save()
+        contact.save(test)
     return render(request,'home/contact.html')
 
 def about(request): 
